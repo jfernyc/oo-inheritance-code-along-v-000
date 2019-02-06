ad4a53c436f55e2d1233460414f663252238794b
J Fern <jfern20@gmail.com>
2019-02-06 05:04:10 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -3,7 +3,7 @@ require_relative "./vehicle.rb"
 class Car < Vehicle  #We use the < to inherit the Car class from Vehicle
   
   def go 
-    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!" #Will invoke if available if not will look in the parent class 
     
   end 
   
